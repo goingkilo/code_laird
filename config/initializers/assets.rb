53808b63c4557eada76c9bfc00ef93467acd21a5
Karthik Raghunathan <kraghunathan@groupon.com>
2016-04-26 21:32:25 +0530

    added bootstrap & jquery assets;upgraded / page to use bootstrap

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,6 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( typogridphy.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( jquery-2.1.3.min.js )
