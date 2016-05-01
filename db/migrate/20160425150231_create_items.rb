e6dd90b2d34cad0480de99975762ad47a1b2920b
goingkilo <goingkilo@gmail.com>
2016-05-01 12:15:29 +0530

    updated migration to skip if db items exists

--- db/migrate/20160425150231_create_items.rb
+++ db/migrate/20160425150231_create_items.rb
@@ -1,10 +1,14 @@
 class CreateItems < ActiveRecord::Migration
   def change
-    create_table :items do |t|
-      t.string :data
-      t.string :project
+	if !table_exists?( :items )
+		create_table :items do |t|
+	    t.string :data
+		t.string :project
 
-      t.timestamps null: false
-    end
+		t.timestamps null: false
+		end
+	else
+		puts " TANLE ITEMS EXISTS"
+	end
   end
 end
